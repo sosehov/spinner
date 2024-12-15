c4e25efcca5e0add0ed03ea6569e80bfdd4095b3
Soseh <sosehv@gmail.com>
2024-12-15 13:40:44 -0500

    Implemented a spinner using setTimeout async function

---
+++ spinner1.js
@@ -0,0 +1,27 @@
+
+setTimeout(() => {
+  process.stdout.write('d2 node spinner1.js...\n');
+}, 200);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 500);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 700);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\\n  '); 
+}, 900);
+
+setTimeout(() => {
+  process.stdout.write('\rd2\n');
+}, 1000);
+
+
